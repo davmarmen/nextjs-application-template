f0765bfe6725e95a881b802b948174ff1b541d10
davmarmen <davidmmendozza@gmail.com>
2025-08-22 19:15:04 +0200

    Add .next/static/chunks/src_app_editor_page_tsx_c4ecceae._.js 1755882902173

---
+++ .next/static/chunks/src_app_editor_page_tsx_c4ecceae._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a231686e._.js",
+  "static/chunks/node_modules_3a7a3639._.js"
+],
+    source: "dynamic"
+});
